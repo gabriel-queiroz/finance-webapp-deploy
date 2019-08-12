dfb02a5df54f6e62c485f49344d23a3a1c7d192a
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-08-11 22:33:37 -0300

    initial commit

---
+++ precache-manifest.dd606a801ecb14446d485de23c6a2dd1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1f59c11331ada797ebcbb526a515f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf38cd47df37e56ceb2b",
+    "url": "/static/css/7.e913cad8.chunk.css"
+  },
+  {
+    "revision": "753e45378d28297e39f7",
+    "url": "/static/css/9.3b233030.chunk.css"
+  },
+  {
+    "revision": "99f790b20d7c0b9f75b6",
+    "url": "/static/js/0.f5bc3183.chunk.js"
+  },
+  {
+    "revision": "4f63d2c62b83135bf04d",
+    "url": "/static/js/1.fd38a4bc.chunk.js"
+  },
+  {
+    "revision": "f6608138efbd95c71e99",
+    "url": "/static/js/10.e42d308f.chunk.js"
+  },
+  {
+    "revision": "f1189fb9919e4ae52c6c",
+    "url": "/static/js/11.14ea899f.chunk.js"
+  },
+  {
+    "revision": "4ea5e00c9da4a22daa0f",
+    "url": "/static/js/12.a66ab9d4.chunk.js"
+  },
+  {
+    "revision": "fd29dcacef1dad5b2b21",
+    "url": "/static/js/13.3659c7d6.chunk.js"
+  },
+  {
+    "revision": "1bbae199c7636cc47e2c",
+    "url": "/static/js/14.f282f4a0.chunk.js"
+  },
+  {
+    "revision": "caaaff3e8ab1c3626771",
+    "url": "/static/js/2.d5bca98d.chunk.js"
+  },
+  {
+    "revision": "c072b9e2d435584575b1",
+    "url": "/static/js/3.16ce8fa2.chunk.js"
+  },
+  {
+    "revision": "8e4d510734cd9e5a91df",
+    "url": "/static/js/4.55225c52.chunk.js"
+  },
+  {
+    "revision": "bf38cd47df37e56ceb2b",
+    "url": "/static/js/7.bc9a56c6.chunk.js"
+  },
+  {
+    "revision": "14e6403c9d3726d8914d",
+    "url": "/static/js/8.33467ed6.chunk.js"
+  },
+  {
+    "revision": "753e45378d28297e39f7",
+    "url": "/static/js/9.8c1722f0.chunk.js"
+  },
+  {
+    "revision": "04838f2dbcf3dc31f962",
+    "url": "/static/js/main.855384e3.chunk.js"
+  },
+  {
+    "revision": "c468829f3bcfd55cc091",
+    "url": "/static/js/runtime~main.1c1c2bde.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
